cc5c7fd1721ac70d93af40b6b785780877183895
Jn_Sajol <julkar10121@gmail.com>
2025-03-24 01:52:10 +0600

    project setup

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,15 +1,26 @@
-//server root file by express
+
 const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
 const PORT = process.env.PORT || 4000;
+
+// const MONGODB_URL = process.env.MONGODB_URL;
+mongoose.connect(process.env.MONGODB_URL, {
+}).then(() => {
+    console.log("✅ Database is connected");
+}).catch(err => {
+    console.error("❌ Cannot connect to the database:", err);
+});
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.listen(PORT, function() {
-    console.log("Server is running on Port: " + PORT);
-}
-);
-
+app.listen(PORT, () => {
+    console.log(`🚀 Server is running on Port: ${PORT}`);
+});
